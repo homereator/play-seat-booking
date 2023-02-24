ddabc1cee2a7ad824136b143296d50e3e3fd1a68
HomeroHernadezNubox <homero.hernandez@externo.nubox.com>
2023-02-24 15:00:06 -0600

    Changes to onlu select 2 seats

--- script.js
+++ script.js
@@ -2,34 +2,22 @@ const container = document.querySelector(".container");
 const seats = document.querySelectorAll(".row .seat:not(.sold)");
 const count = document.getElementById("count");
 const total = document.getElementById("total");
-const movieSelect = document.getElementById("movie");
-
-populateUI();
-
-let ticketPrice = +movieSelect.value;
-
-// Save selected movie index and price
-function setMovieData(movieIndex, moviePrice) {
-  localStorage.setItem("selectedMovieIndex", movieIndex);
-  localStorage.setItem("selectedMoviePrice", moviePrice);
-}
 
 // Update total and count
 function updateSelectedCount() {
   const selectedSeats = document.querySelectorAll(".row .seat.selected");
 
-  const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
-
-  localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
-
+  // Count only the selected seats
   const selectedSeatsCount = selectedSeats.length;
 
+  // Set the text content of the count and total elements
   count.innerText = selectedSeatsCount;
-  total.innerText = selectedSeatsCount * ticketPrice;
+  total.innerText = selectedSeatsCount;
 
-  setMovieData(movieSelect.selectedIndex, movieSelect.value);
+  // Save the selected seats data to local storage
+  const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
+  localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
 }
-
 
 // Get data from localstorage and populate UI
 function populateUI() {
@@ -38,34 +26,31 @@ function populateUI() {
   if (selectedSeats !== null && selectedSeats.length > 0) {
     seats.forEach((seat, index) => {
       if (selectedSeats.indexOf(index) > -1) {
-        console.log(seat.classList.add("selected"));
+        seat.classList.add("selected");
+      } else {
+        seat.classList.add("available"); // Add "available" class to non-selected seats
       }
     });
-  }
-
-  const selectedMovieIndex = localStorage.getItem("selectedMovieIndex");
-
-  if (selectedMovieIndex !== null) {
-    movieSelect.selectedIndex = selectedMovieIndex;
-    console.log(selectedMovieIndex)
+  } else {
+    seats.forEach((seat) => {
+      seat.classList.add("available"); // Add "available" class to all seats if no data in local storage
+    });
   }
 }
 console.log(populateUI())
-// Movie select event
-movieSelect.addEventListener("change", (e) => {
-  ticketPrice = +e.target.value;
-  setMovieData(e.target.selectedIndex, e.target.value);
-  updateSelectedCount();
-});
 
 // Seat click event
 container.addEventListener("click", (e) => {
-  if (
-    e.target.classList.contains("seat") &&
-    !e.target.classList.contains("sold")
-  ) {
-    e.target.classList.toggle("selected");
-
+  if (e.target.classList.contains("seat") && !e.target.classList.contains("sold")) {
+    if (e.target.classList.contains("selected")) {
+      e.target.classList.remove("selected");
+    } else {
+      // Limit the selection to two seats
+      const selectedSeats = document.querySelectorAll(".row .seat.selected");
+      if (selectedSeats.length < 2) {
+        e.target.classList.add("selected");
+      }
+    }
     updateSelectedCount();
   }
 });
